0247428b12514483f10b77ffdfcfd3acc3b054b9
Fatma Solmaz <solmazfatma967@gmail.com>
2024-05-16 16:59:50 +0300

    ana galeri yapıldı

--- galeri.php
+++ galeri.php
@@ -22,41 +22,72 @@
                 <li><a href="galeri.php" class="page-link active">Galeri</a></li>
             </ul>
         </div>
-
-
-        <div class="container">
-         <div class="card__container">
-            <article class="card__article">
-               <img src="assets/img/user-1.png" alt="image" class="card__img">
-
-               <div class="card__data">
-                  <span class="card__description">Vancouver Mountains, Canada</span>
-                  <h2 class="card__title">The Great Path</h2>
-                  <a href="#" class="card__button">Read More</a>
-               </div>
-            </article>
-
-            <article class="card__article">
-               <img src="assets/img/user-1.png" alt="image" class="card__img">
-
-               <div class="card__data">
-                  <span class="card__description">Poon Hill, Nepal</span>
-                  <h2 class="card__title">Starry Night</h2>
-                  <a href="#" class="card__button">Read More</a>
-               </div>
-            </article>
-
-            <article class="card__article">
-               <img src="assets/img/user-1.png" alt="image" class="card__img">
-
-               <div class="card__data">
-                  <span class="card__description">Bojcin Forest, Serbia</span>
-                  <h2 class="card__title">Path Of Peace</h2>
-                  <a href="#" class="card__button">Read More</a>
-               </div>
-            </article>
+    </div>
+   
+    <div class="card-container">
+      <div class="card">
+         <img src="assets/img/user-1.png">
+         <div class="card-content">
+            <h1>Firmamız</h1>
+            <p>Firmamız ile ilgili görselleri incelemek için
+               tıklayınız
+            </p>
+            <a href=""class="card-button">Tıklayınız</a>
          </div>
       </div>
+      <div class="card">
+         <img src="assets/img/user-1.png">
+         <div class="card-content">
+            <h1>Firmamız</h1>
+            <p>Firmamız ile ilgili görselleri incelemek için
+               tıklayınız
+            </p>
+            <a href=""class="card-button">Tıklayınız</a>
+         </div>
+      </div>
+      <div class="card">
+         <img src="assets/img/user-1.png">
+         <div class="card-content">
+            <h1>Firmamız</h1>
+            <p>Firmamız ile ilgili görselleri incelemek için
+               tıklayınız
+            </p>
+            <a href=""class="card-button">Tıklayınız</a>
+         </div>
+      </div>
+      <div class="card">
+         <img src="assets/img/user-1.png">
+         <div class="card-content">
+            <h1>Firmamız</h1>
+            <p>Firmamız ile ilgili görselleri incelemek için
+               tıklayınız
+            </p>
+            <a href=""class="card-button">Tıklayınız</a>
+         </div>
+      </div>
+      <div class="card">
+         <img src="assets/img/user-1.png">
+         <div class="card-content">
+            <h1>Firmamız</h1>
+            <p>Firmamız ile ilgili görselleri incelemek için
+               tıklayınız
+            </p>
+            <a href=""class="card-button">Tıklayınız</a>
+         </div>
+      </div>
+      <div class="card">
+         <img src="assets/img/user-1.png">
+         <div class="card-content">
+            <h1>Firmamız</h1>
+            <p>Firmamız ile ilgili görselleri incelemek için
+               tıklayınız
+            </p>
+            <a href=""class="card-button">Tıklayınız</a>
+         </div>
+      </div>
+    </div>
+     
+        
 
 
 
